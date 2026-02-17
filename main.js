57cc8a3159d8d22c9c94ff33d42da24bdd8d771b
enzae69 <enzaeenzae5@gmail.com>
2026-02-17 15:11:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALbCmk9pVv5WUc1w9Zf9fSHshjhEc7UOw",
+  authDomain: "sudoku-multiplayer-18534.firebaseapp.com",
+  projectId: "sudoku-multiplayer-18534",
+  storageBucket: "sudoku-multiplayer-18534.firebasestorage.app",
+  messagingSenderId: "401519012394",
+  appId: "1:401519012394:web:41470fa60f037bd73c7153",
+  measurementId: "G-1B6EV80YYX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
